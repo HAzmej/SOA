fbd59c026dd885490b23500a96fc17764e64b52b
HAzmej <86357096+HAzmej@users.noreply.github.com>
2024-12-20 16:18:53 +0100

    Update MissionRessource.java

--- MissionMicroService/src/main/java/fr/insa/micro/Mission_MicroService/controlleur/MissionRessource.java
+++ MissionMicroService/src/main/java/fr/insa/micro/Mission_MicroService/controlleur/MissionRessource.java
@@ -22,10 +22,6 @@ import fr.insa.micro.Mission_MicroService.model.Mission;
 @RestController
 @RequestMapping("/missions")
 public class MissionRessource {
-	@GetMapping("/testAzure")
-	public String testazure(){
-		return "Le test de deploiement d Azure";
-	}
 	@Value("${server.port}")
 	private String serverPort;
 	
